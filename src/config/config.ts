99d673a60a6c73f90c99aa8d013e677768d2ddd6
André L Reis <gato5andre26@gmail.com>
2024-05-09 17:05:53 +0100

    fix: config

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,7 +11,7 @@ const config = {
   },
 
   apiRoutes: {
-    base: process.env.NODE_ENV === "production" ? "https://" + process.env.VERCEL_URL : (process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api"),
+    base: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
     routes: {
       upload: "/upload",
       delete: "/delete",
